10711836ac9fb9ae2b9e6cee743cd6743f1cc2e9
collin-mccarter <collinmccarter12@gmail.com>
2023-05-28 17:16:26 -0500

    added seed files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,28 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Blog, Comment } = require('../models');
 
 const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
-  });
+  })
+
+  for (const blog of blogData) {
+    await Blog.create ({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    })
+  }
+
+  const comments = await Comment.bulkCreate(commentData, {
+    returning: true,
+  })
 
   process.exit(0);
 };
