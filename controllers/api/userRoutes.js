042e7ad1d700da804affeddbaed9c299bca5c333
collin-mccarter <collinmccarter12@gmail.com>
2023-06-01 16:52:57 -0500

    debugging purposes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -45,8 +45,11 @@ router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
+    console.log(userData)
+
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.username = userData.username
       req.session.logged_in = true;
 
       res.status(200).json(userData);
